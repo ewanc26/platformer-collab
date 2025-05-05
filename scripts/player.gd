5f6376ecc1d8cb7dcd1a576969ec22d975b6841f
Ewan <git@ewancroft.uk>
2025-05-05 21:52:09 +0100

    feat(player): add movement input handling for player
    Implement movement input handling in the player script to allow the player to move left, right, forward, and backward using keyboard input. This change enables basic player movement in the game.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,4 +4,7 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	var input := Vector3.ZERO
+	
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_backwards")
