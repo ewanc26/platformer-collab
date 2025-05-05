638fdc40d8a7d48ee8f03bb8f318669a34875008
Ewan <git@ewancroft.uk>
2025-05-05 22:09:13 +0100

    feat(player): capture mouse on ready and release on ui_cancel
    Capture the mouse cursor when the player is ready to ensure proper camera control. Release the mouse cursor when the "ui_cancel" action is pressed to allow the player to interact with the UI

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends RigidBody3D
 
 func _ready() -> void:
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta: float) -> void:
 	var input := Vector3.ZERO
@@ -9,4 +9,7 @@ func _process(delta: float) -> void:
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_backwards")
 	
-	apply_central_force(input * 1200.0 * delta) 
+	apply_central_force(input * 1200.0 * delta)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
